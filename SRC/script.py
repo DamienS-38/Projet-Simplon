82611108588f1c6956a1930d93c384ada4b46801
Dalien Schaeffer <dschaeffer@cfdp.fr>
2025-04-14 15:54:23 +0200

    Commit préparation base de données

--- SRC/script.py
+++ SRC/script.py
@@ -2,12 +2,79 @@
 import pandas as pd
 import sqlite3
 
-# Connexion à la base de données SQLite "pme"
+# Connexion à la base SQLite
 conn = sqlite3.connect("pme.db")
 cursor = conn.cursor()
 
+# Création des tables si elles n'existent pas
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Produit (
+    id_produit TEXT PRIMARY KEY,
+    nom TEXT,
+    prix REAL,
+    stock INT
+)
+''')
 
-#csv to df
-magasins=pd.read_csv('Data/magasins.csv', delimiter=',', index_col=0)
-produits=pd.read_csv('Data/produits.csv', delimiter=',', index_col=1)
-ventes=pd.read_csv('Data/ventes.csv', delimiter=',')
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Magasins (
+    id_magasin TEXT PRIMARY KEY,
+    ville TEXT,
+    Nb_salarie INT
+)
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Ventes (
+    id_vente INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_produit TEXT,
+    id_magasin TEXT,
+    date_vente TEXT,
+    quantite INTEGER,
+    FOREIGN KEY(id_produit) REFERENCES Produit(id_produit),
+    FOREIGN KEY(id_magasin) REFERENCES Magasins(id_magasin)
+)
+''')
+
+# Chargement des CSV
+df_produits =pd.read_csv('Data/produits.csv', delimiter=',')
+df_magasins = pd.read_csv('Data/magasins.csv', delimiter=',')
+df_ventes=pd.read_csv('Data/ventes.csv', delimiter=',')
+
+
+#renommer les colonnes des DataFrame
+df_produits = df_produits.rename(columns={
+    'ID Référence produit':'id_produit',
+    'Nom': 'nom',
+    'Prix': 'prix',
+    'Stock': 'stock'
+})
+ordre_colonnes_produits = ['id_produit', 'nom', 'prix', 'stock']
+df_produits = df_produits[ordre_colonnes_produits]
+
+df_magasins= df_magasins.rename(columns={
+    'ID Magasin': 'id_magasin',
+    'Ville' : 'ville',
+    'Nombre de salariés': 'nb_salarie'
+})
+
+df_ventes=df_ventes.rename(columns={
+    'ID Magasin': 'id_magasin',
+    'ID Référence produit':'id_produit',
+    'Date': 'date_vente',
+    'Quantité': 'quantite'
+})
+ordre_colonnes_ventes = ['id_produit', 'id_magasin', 'date_vente', 'quantite']
+df_ventes = df_ventes[ordre_colonnes_ventes]
+
+
+# Insertion des données en dans la base de données SQlite
+df_produits.to_sql('Produit', conn, if_exists='append', index=False)
+df_magasins.to_sql('Magasins', conn, if_exists='append', index=False)
+df_ventes.to_sql('vente', conn, if_exists='append', index=False)
+
+
+
+
+conn.commit()
+conn.close()
